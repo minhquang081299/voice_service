81c02bff31545b7081f98942075cbc5ca2a99024
hoangnv172566 <nguyenhoang8399@gmail.com>
2022-01-21 08:03:21 +0700

    first commit

--- src/run.py
+++ src/run.py
@@ -1,8 +1,10 @@
 from flask import config
 from src.main import app 
 from src.api.config.config import Config as cf
+from waitress import serve
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(port=cf.PORT,
-            host=cf.HOST)
+    
+    serve(app, host=cf.HOST, port = cf.PORT)
+   
